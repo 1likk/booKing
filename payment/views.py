80afd8bc6336a2e6752cf85dfef2d134aa468804
Birlik Zhumadil <zhbirlik.2006@gmail.com>
2026-01-27 15:27:40 +0500

    Fix Stripe API version issue

--- payment/views.py
+++ payment/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 import stripe 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY 
-stripe.api_version = settings.STRIPE_API_VERSION
+# stripe.api_version = settings.STRIPE_API_VERSION  # Используем версию по умолчанию
 
 def payment_process(request):
     order_id = request.session.get('order_id', None)

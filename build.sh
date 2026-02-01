f0b3aeb27c1de505d3bd32ff60732da952dc7406
Birlik Zhumadil <zhbirlik.2006@gmail.com>
2026-02-01 15:17:07 +0500

    Добавлено авто создание суперпользователя

--- build.sh
+++ build.sh
@@ -6,3 +6,14 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Создание суперпользователя (если не существует)
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'zhbirlik.2006@gmail.com', 'Baris123!')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+EOF
